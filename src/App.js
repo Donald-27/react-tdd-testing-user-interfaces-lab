3c2cd8876d2cd844370c00421c13ade8bc632529
Donald Kiprop <kipropdonald27@gmail.com>
2025-04-17 13:31:18 +0300

    added test file code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Donald</h1>
+
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Photo of Alex smiling outdoors"
+      />
+
+      <h2>About Me</h2>
+      <p>
+        I'm a passionate developer who loves building cool stuff with React and solving problems with code.
+      </p>
+
+      <a href="https://github.com/Donald-27" target="_blank" rel="noreferrer">
+        GitHub
+      </a>
+      <br />
+      <a href="https://linkedin.com/in/kiprop-donald-56b898352" target="_blank" rel="noreferrer">
+        LinkedIn
+      </a>
+    </div>
+  );
 }
 
 export default App;
